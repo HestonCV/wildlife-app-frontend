47787d15403e73be9dd4327df35d9979309192bd
Heston Vaughan <heston.cv@gmail.com>
2023-08-11 05:33:54 -0500

    set up request from server for image that is updated on reload

--- wildlife/App.js
+++ wildlife/App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Image,
+  Button,
+  Text,
+  View,
+  SafeAreaView,
+  StyleSheet,
+} from "react-native";
+
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [imageURL, setImageURL] = useState(null);
+  const [reload, setReload] = useState(false);
+
+  const fetchData = () => {
+    fetch("http://192.168.1.145:5000/image")
+      .then((response) => {
+        setImageURL(response.url);
+      })
+      .catch((error) => console.error(error));
+    console.log("test");
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [reload]);
+
   return (
-    <View style={styles.container}>
-      <Text>testing</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      {imageURL && <Image source={{ uri: imageURL }} style={styles.image} />}
+      {/*       <Button
+        title="Reload"
+        onPress={() => {
+          fetchData();
+          setReload(!reload);
+        }}
+      /> */}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#97B9C8",
+    height: "100%",
     alignItems: "center",
     justifyContent: "center",
+  },
+  topView: {
+    height: "30%",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  loginText: {
+    fontSize: 20,
+  },
+  buttonContainer: {
+    paddingTop: 50,
+  },
+  loginButton: {},
+  image: {
+    width: 400,
+    height: 400,
   },
 });
