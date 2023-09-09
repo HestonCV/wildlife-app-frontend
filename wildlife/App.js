20b789895c862d5e152dfea18a54d9777aff5ddc
Heston Vaughan <heston.cv@gmail.com>
2023-09-09 17:40:48 -0500

    add initial authentication check of token stored with expo secure store

--- wildlife/App.js
+++ wildlife/App.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as SecureStore from "expo-secure-store";
 import { NavigationContainer } from "@react-navigation/native";
 import MainTabs from "./src/navigation/MainTabs";
 import AuthStack from "./src/navigation/AuthStack";
 
 const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
   // TODO: real authentication call to server will go here
-  const isAuthenticated = true;
+  useEffect(() => {
+    const fetchTokenAndValidate = async () => {
+      try {
+        const token = await SecureStore.getItemAsync("user_token");
+        if (token) {
+          const response = await fetch(
+            "http://192.168.1.145:5000/validate_token",
+            {
+              method: "POST",
+              headers: { Authorization: `Bearer ${token}` },
+            }
+          );
+          const data = await response.json();
+          setIsAuthenticated(data.data.authorized);
+        } else {
+          setIsAuthenticated(false);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchTokenAndValidate();
+  }, []);
 
   return (
     <NavigationContainer>
